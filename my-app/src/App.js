c23fb870bf6df66ac877ae6d1fa0e33b036ca5aa
logralahad <logralahad@gmail.com>
2022-05-13 18:58:28 -0500

    Rutas publicas y privadas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
+import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-
+import MenuOpciones from "./menuOpciones";
+import { Link, Switch, BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import PublicLayout from "./components/Public/PublicLayout";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
 
 function App() {
   return (
@@ -11,16 +13,20 @@ function App() {
         {/* Rutas publicas */}
         <Route element={<PublicLayout />}>
           <Route path="/" element={<div>Inicio</div>} />
+          <Route path="login" element={<div>Login</div>} />
+          {/* Por si no existe la ruta */}
+          <Route path="*" element={<div>No encontrada</div>} />
+        </Route>
+
+        {/* Rutas protegidas */}
+        <Route element={<ProtectedRoutes />}>
+          <Route index path="/" element={<div>Inicio</div>} />
           <Route path="tablero" element={<div>Admin</div>} />
           <Route path="perfil" element={<div>Perfil</div>} />
           <Route path="diario" element={<div>Diario</div>} />
           <Route path="examenes" element={<div>Examenes</div>} />
           <Route path="recomendaciones" element={<div>Recomendaciones</div>} />
-          {/* Por si no existe la ruta */}
-          <Route path="*" element={<div>No encontrada</div>} />
         </Route>
-        <Route path="login" element={<div>Login</div>} />
-        {/* Rutas protegidas */}
       </Route>
     </Routes>
   );
---
+++ my-app/src/components/Public/PublicFooter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const PublicFooter = () => {
+  return (
+    <div id="page" data-swup={0} className="device-is-ios">
+      <div id="footer-bar" className="footer-bar-1">
+        <a href="/login" className="active-nav">
+          <i className="fa fa-home" />
+          <span>Iniciar sesi&oacute;n</span>
+        </a>
+        <a href="/registrarse">
+          <i className="fa fa-star" />
+          <span>Registrarse</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default PublicFooter;
