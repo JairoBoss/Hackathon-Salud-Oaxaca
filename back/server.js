fc80efb65f095b25434ea2864b206498e701ef9e
JairoBoss <jairoboss00@gmail.com>
2022-05-14 12:25:30 -0500

    s3 added

--- back/server.js
+++ back/server.js
@@ -6,6 +6,26 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./src/models");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const aws = require("aws-sdk");
+const randomBytes = promisify(crypto.randomBytes);
+
+const region = process.env.AWS_REGION;
+const bucketName = process.env.AWS_BUCKET_NAME;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccesKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new aws.S3({
+  //apiVersion: '2006-03-01',
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccesKey,
+  region: region,
+  signatureVersion: "v4",
+});
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -49,14 +69,54 @@ app.get("/", (req, res) => {
   res.json({ message: "Bienvenido" });
 });
 
-require("./src/routes/usuario.routes")(app)
-require("./src/routes/evento.routes")(app)
-require("./src/routes/medicamento.routes")(app)
-require("./src/routes/enfermedad.routes")(app)
-require("./src/routes/pregunta.routes")(app)
-require("./src/routes/examen.routes")(app)
-require("./src/routes/perfilMedico.routes")(app)
-require("./src/routes/habito.routes")(app)
+require("./src/routes/usuario.routes")(app);
+require("./src/routes/evento.routes")(app);
+require("./src/routes/medicamento.routes")(app);
+require("./src/routes/enfermedad.routes")(app);
+require("./src/routes/pregunta.routes")(app);
+require("./src/routes/examen.routes")(app);
+require("./src/routes/perfilMedico.routes")(app);
+require("./src/routes/habito.routes")(app);
+
+app.post("/api/s3Url", upload.single("foto"), async (req, res) => {
+  const stream = fs.createReadStream(req.file.path);
+
+  const ext = path.extname(req.file.originalname).toLowerCase();
+
+  let fileType = "";
+
+  if (ext == ".png") {
+    fileType = "image/png";
+  } else if (ext == ".jpg" || ext == ".jpeg") {
+    fileType = "image/jpg";
+  } else {
+    res.send({ data: "error" });
+  }
+
+  stream.on("error", function (err) {
+    console.log("error in read stream: ", err);
+    throw err;
+  });
+
+  const rawBytes = await randomBytes(16);
+  const imageName = rawBytes.toString("hex");
+
+  let params = {
+    Bucket: bucketName,
+    Body: stream,
+    Key: imageName,
+    ContentType: fileType,
+  };
+  const data = await s3.upload(params).promise();
+
+  res.send({ data: data.Key });
+});
+
+app.get("/api/s3Url2/:key", async (req, res) => {
+  const key = req.params.key;
+  const imagen = await getImagen(key);
+  res.send(imagen);
+});
 
 app.listen(PORT, () => {
   console.log(`Server en el puerto ${PORT}`);
