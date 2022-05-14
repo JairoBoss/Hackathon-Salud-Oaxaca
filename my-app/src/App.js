752aa9b14feff85ed50e5680e3fe9daeee2157b4
logralahad <logralahad@gmail.com>
2022-05-13 19:18:56 -0500

    Ruta corregida

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MenuOpciones from "./menuOpciones";
-import { Link, Switch, BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
-import PublicLayout from "./components/Public/PublicLayout";
-import { AuthContext } from "./context/AuthContext";
-import { useContext, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   const publicRoutes = (
     <Routes>
-      <Route path="/" element={<div >Login</div>} />
-      <Route path="/registro" element={<div >Registro</div>} />
+      <Route path="/login" element={<div>Login</div>} />
+      <Route path="/registro" element={<div>Registro</div>} />
       <Route path="*" element={<div>No encontrada</div>} />
     </Routes>
   );
 
   const userRoutes = (
     <Routes>
-      {/* <Route path="/" element={<Layout></Layout>}> */}
-      {/* Rutas publicas */}
-      {/* <Route element={<PublicLayout />}> */}
       <Route path="/" element={<div>Inicio</div>} />
       <Route path="tablero" element={<div>Admin</div>} />
       <Route path="perfil" element={<div>Perfil</div>} />
@@ -29,10 +19,6 @@ function App() {
       <Route path="examenes" element={<div>Examenes</div>} />
       <Route path="recomendaciones" element={<div>Recomendaciones</div>} />
       <Route path="*" element={<div>No encontrada</div>} />
-      {/* Por si no existe la ruta */}
-
-      {/* </Route> */}
-      {/* </Route> */}
     </Routes>
   );
 
@@ -42,12 +28,7 @@ function App() {
     return publicRoutes;
   };
 
-  return (
-    <BrowserRouter>      
-      {getRoutes()}
-    </BrowserRouter>
-
-  );
+  return <BrowserRouter>{getRoutes()}</BrowserRouter>;
 }
 
 export default App;
