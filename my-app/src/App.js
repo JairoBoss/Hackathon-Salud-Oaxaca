d72c4de9b922454842a87f5e1ff554a839894ed1
logralahad <logralahad@gmail.com>
2022-05-15 10:12:18 -0500

    Vista de tratamiento agregada

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,16 +8,12 @@ import Login from "./components/public/Login";
 import Register from "./pages/public/Register";
 import AddEnfermedad from "./components/private/AddEnfermedad";
 import Perfil from "./components/private/Perfil";
-// import Examenes from "./components/public/quizzes";
+import Examenes from "./components/public/quizzes";
 import Estudios from "./components/private/Examenes";
 import { AuthContext } from "./context/AuthContext";
-<<<<<<< HEAD
-import Diario from "./components/private/Diario";
-import AgregarDiario from "./components/private/AgregarDiario";
-import Examenes from "./components/public/quizzes/index";
-=======
 import EditProfile from "./pages/public/EditProfile";
->>>>>>> e94ee21d87827e65a22e17fd7d54c080eeca42f8
+import Diario from "./components/private/Diario";
+import Tratamiento from "./components/private/Tratamiento";
 
 function App() {
   const { currentUser, checkUser } = useContext(AuthContext);
@@ -34,17 +30,12 @@ function App() {
     <Route element={<PrivateLayout />}>
       <Route index path="/" element={<Perfil />} />
       <Route path="/enfermedades" element={<AddEnfermedad />} />
-<<<<<<< HEAD
-      <Route path="/diario" element={<Diario/>} />
-      <Route path="/agregarDiario" element={<AgregarDiario/>} />      
-      <Route path="/estudios" element={<Estudios />} />
-=======
       <Route path="/editar-perfil" element={<EditProfile />} />
-      <Route path="/diario" element={<div>Diario</div>} />
->>>>>>> e94ee21d87827e65a22e17fd7d54c080eeca42f8
+      <Route path="/diario" element={<Diario />} />
       <Route path="/examenes" element={<Examenes />} />
       <Route path="recomendaciones" element={<div>Recomendaciones</div>} />
-      <Route path="tratamiento" element={<div>Tratamiento</div>} />
+      <Route path="/tratamiento" element={<Tratamiento />} />
+      <Route path="/estudios" element={<Estudios />} />
       <Route path="*" element={<div>No encontrada</div>} />
     </Route>
   );
---
+++ my-app/src/components/private/Tratamiento.js
@@ -0,0 +1,211 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import MedicamentosService from "../../services/Medicamentos.Service";
+import PerfilMedicoService from "../../services/PerfilMedico.Service";
+import Header from "../Header";
+
+import { Formik, Form, Field } from "formik";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCapsules,
+  faFilePrescription,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { isEmpty } from "../../validations/validations";
+
+const Tratamiento = () => {
+  const [meds, setMeds] = useState([]);
+  const [file, setFile] = useState("");
+  const [idFile, setIdFile] = useState("");
+  const { currentUser } = useContext(AuthContext);
+
+  const getMeds = () => {
+    PerfilMedicoService.getByUserId(currentUser._id)
+      .then((response) => {
+        setIdFile(response[0]._id);
+        var meds = response[0].Medicamentos;
+        for (let i = 0; i < meds.length; i++) {
+          MedicamentosService.get(meds[i])
+            .then((medDesc) => {
+              setMeds((meds) => [...meds, medDesc]);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (meds.length === 0) getMeds();
+  }, []);
+
+  return (
+    <>
+      <Header titulo={"Tratamiento actual"}></Header>
+      <div className="page-content header-clear-medium">
+        <Formik
+          initialValues={{
+            Nombre: "",
+            Dosis: "",
+            Frecuencia: "",
+            Foto: "",
+          }}
+          onSubmit={(values, actions) => {
+            MedicamentosService.create(values)
+              .then((response) => {
+                meds.push(response);
+                let user = {
+                  Medicamentos: meds,
+                };
+                PerfilMedicoService.update(idFile, user)
+                  .then((medAdded) => {
+                    actions.resetForm();
+                    setMeds((meds) => [...meds, medAdded]);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className="card card-style">
+                <div className="content mb-0">
+                  <div className="text-center mb-3">
+                    <h2>Agrega tus medicamentos</h2>
+                    <p className="mb-4">Para cuidar de tu salud</p>
+                  </div>
+
+                  <div className="input-style input-style-always-active has-borders has-icon">
+                    <i className="fa">
+                      <FontAwesomeIcon icon={faCapsules} fontSize={12} />
+                    </i>
+                    <Field
+                      name="Nombre"
+                      className="form-control"
+                      validate={isEmpty}
+                    />
+                    <label
+                      htmlFor="Nombre"
+                      className={
+                        errors.Nombre && touched.Nombre
+                          ? "color-red-dark font-13"
+                          : "color-blue-dark font-13"
+                      }
+                    >
+                      Nombre(s) *
+                    </label>
+                  </div>
+
+                  <div className="input-style input-style-always-active has-borders has-icon">
+                    <i className="fa">
+                      <FontAwesomeIcon
+                        icon={faFilePrescription}
+                        fontSize={12}
+                      />
+                    </i>
+                    <Field
+                      name="Dosis"
+                      className="form-control"
+                      validate={isEmpty}
+                    />
+                    <label
+                      htmlFor="Dosis"
+                      className={
+                        errors.Dosis && touched.Dosis
+                          ? "color-red-dark font-13"
+                          : "color-blue-dark font-13"
+                      }
+                    >
+                      Dosis *
+                    </label>
+                  </div>
+
+                  <div className="input-style input-style-always-active has-borders has-icon">
+                    <i className="fa">
+                      <FontAwesomeIcon icon={faClock} fontSize={12} />
+                    </i>
+                    <Field
+                      name="Frecuencia"
+                      className="form-control"
+                      validate={isEmpty}
+                    />
+                    <label
+                      htmlFor="Frecuencia"
+                      className={
+                        errors.Frecuencia && touched.Frecuencia
+                          ? "color-red-dark font-13"
+                          : "color-blue-dark font-13"
+                      }
+                    >
+                      Frecuencia *
+                    </label>
+                  </div>
+                  <div className="file-data pb-5">
+                    <input
+                      type="file"
+                      id="file-upload"
+                      className="upload-file bg-green-dark shadow-s rounded-s"
+                      accept="image/*"
+                      onChange={(e) => setFile(e.target.files[0])}
+                    />
+                    <p className="upload-file-text color-white">
+                      Agregar imagen
+                    </p>
+                  </div>
+                  <a className="btn btn-full font-900 text-uppercase bg-blue-dark btn-m rounded-sm mb-3">
+                    <button type="submit" width={"100%"}>
+                      Agregar medicamento
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+        <div className="calendar bg-theme shadow-xl rounded-m">
+          <div className="cal-footer">
+            <h6 className="cal-sub-title uppercase bold bg-highlight color-white">
+              Medicamentos actuales
+            </h6>
+          </div>
+          <div className="content">
+            {meds &&
+              meds.map((medicamento, index) => {
+                return (
+                  <div key={index} className="row justify-content-center">
+                    <div className="col-3">
+                      <img
+                        src="https://m.media-amazon.com/images/I/81aylYdIRPL._AC_SL1500_.jpg"
+                        className="rounded-s mx-auto mt-1"
+                        width={65}
+                      />
+                    </div>
+                    <div className="col-9 ps-0">
+                      <h4>{medicamento.Nombre}</h4>
+                      <p className="mt-n1 line-height-m">
+                        Frecuencia: {medicamento.Frecuencia}
+                        <br />
+                        Dosis: {medicamento.Dosis}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tratamiento;
